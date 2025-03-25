cc75a6880eb6bc9c0e639222e83fef6533e73e06
Sno <ochoa.sara2036@gmail.com>
2025-03-24 17:38:10 -0700

    wave 01 progress so far

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,39 @@
+movies_dict = {}
+
+user_data = {"watched": [],
+            "watchlist" : []}
+
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title or genre or rating is False:
+        return None
+    
+    movies_dict["title"] = title
+    movies_dict["genre"] = genre
+    movies_dict["rating"] = rating
+
+    return movies_dict
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    # remove from watch list and add to watched
+    if title not in user_data:
+        return user_data
+    
+    elif title in user_data["watchlist"]:
+        user_data["watchlist"].remove(title)
+        user_data["watched"].append(title)
+        
+        return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
