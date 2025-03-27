7371a236ba49e6a5f2d2f563cae6494ab0ad81a1
Dana <da.alshaar@gmail.com>
2025-03-27 13:17:54 -0700

    wave2 1 test fixed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -59,8 +59,8 @@ def get_watched_avg_rating(user_data):
     average = rating_total / len(user_data["watched"])
     return average
     
-def most_watched_genre(user_data):
-    if len(user_data["watched"]) == None:
+def get_most_watched_genre(user_data):
+    if len(user_data["watched"]) == 0.0:
         return None
     genre_count = {}
     for movie in user_data["watched"]:
@@ -120,10 +120,23 @@ def get_friends_unique_watched(user_data):
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
-# 
-# 
+# create recommended movie list = []
+# loop over friends(watched-movies)
+# compare our supscriptions(host) if ours match a s friend supscrirption
+# add this movie to our empty list
+# loop through user watched and compare to recommended movies
+# remove matches from what we seen to what has een recommended
+# return updated recommended movie list
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
-# -----------------------------------------
+# determain most frequant genre, loop through list of movies
+# count how much genre appeared and return genre:key, value:counter.
+# save that list as varaible
+# create empty list recommended movies
+# make copy of friends movie list, loop through it and remove each movie that has a match
+# with title we have seen
+# loop through list of movie and remove movie that its genre doesnt match with genre variable
+# return updated list of recommended movies
+#  
 
